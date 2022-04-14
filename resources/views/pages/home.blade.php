188ce7195ac0e94ac3da363e32773489b409d012
Dũng Phạm <pndungtb@gmail.com>
2022-04-14 22:00:01 +0700

    web quan ao

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layout')
+@section('content')
+<div class="features_items"><!--features_items-->
+						<h2 class="title text-center">Sản phẩm mới</h2>
+                        @foreach($all_product as $key => $product)
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+										<div class="productinfo text-center">
+											<img src="{{URL::to('public/upload/product/'.$product->product_image)}}" alt="" />
+                                            <h2>{{number_format((float)$product->product_price).' '.'VND'}}</h2>
+											<p>{{($product->product_name)}}</p>
+											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm vào giỏ hàng</a>
+										</div>
+								</div>
+								<div class="choose">
+									<ul class="nav nav-pills nav-justified">
+										<li><a href="#"><i class="fa fa-plus-square"></i>Thêm vào danh sách yêu thích</a></li>
+										<li><a href="#"><i class="fa fa-plus-square"></i>Thêm vào để so sánh</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+                        @endforeach
+
+</div><!--features_items-->
+@endsection
